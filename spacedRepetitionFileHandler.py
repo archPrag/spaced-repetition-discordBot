d593231860f1cd6b1a8902658a7a9978b93c402c
archPrag <arthur.prager.ea@gmail.com>
2024-01-05 05:12:52 -0300

    Translated the theoretical exercise get function corrected some spelling mistakes

--- spacedRepetitionFileHandler.py
+++ spacedRepetitionFileHandler.py
@@ -1,169 +1,267 @@
 # coding: utf-8
-import time
 import os
+import time
+
 
 def initialiseFiles():
-    #Ensures the existence of data files
-    numericQuestionFile = open(".spacedRepetition/data/numericQuestions","a",encoding="utf-8")
-    numericDataFile = open(".spacedRepetition/data/numericData","a",encoding="utf-8")
+    # Ensures the existence of data files
+    numericQuestionFile = open(
+        ".spacedRepetition/data/numericQuestions", "a", encoding="utf-8"
+    )
+    numericDataFile = open(".spacedRepetition/data/numericData", "a", encoding="utf-8")
     numericQuestionFile.close()
     numericDataFile.close()
-    teoricQuestionFile = open(".spacedRepetition/data/teoricQuestions","a",encoding="utf-8")
-    teoricDataFile = open(".spacedRepetition/data/teoricData","a",encoding="utf-8")
-    teoricAnswerFile = open(".spacedRepetition/data/teoricAnswers","a",encoding="utf-8")
-    teoricQuestionFile.close()
-    teoricAnswerFile.close()
-    teoricDataFile.close()
-    helpFile = open("help.txt","a",encoding="utf-8")
+    theoreticalQuestionFile = open(
+        ".spacedRepetition/data/teoricQuestions", "a", encoding="utf-8"
+    )
+    theoreticalDataFile = open(
+        ".spacedRepetition/data/teoricData", "a", encoding="utf-8"
+    )
+    theoreticalAnswerFile = open(
+        ".spacedRepetition/data/teoricAnswers", "a", encoding="utf-8"
+    )
+    theoreticalQuestionFile.close()
+    theoreticalAnswerFile.close()
+    theoreticalDataFile.close()
+    helpFile = open("help.txt", "a", encoding="utf-8")
     helpFile.close
-def getNumericExercise():
-    #Get the questions
-    numericQuestionFile = open(".spacedRepetition/data/numericQuestions","a",encoding="utf-8")
+
+
+def getNumericProblems():
+    # Get the questions
+    numericQuestionFile = open(
+        ".spacedRepetition/data/numericQuestions", "a", encoding="utf-8"
+    )
     questions = numericQuestionFile.read()
-    questions=questions.split("\n")
+    questions = questions.split("\n")
     numericQuestionFile.close()
-    #Get the Data
-    numericDataFile = open(".spacedRepetition/data/numericData","a",encoding="utf-8")
-    data=numericDataFile.read()
-    data=data.split("\n")
+    # Get the Data
+    numericDataFile = open(".spacedRepetition/data/numericData", "a", encoding="utf-8")
+    data = numericDataFile.read()
+    data = data.split("\n")
     for index in range(len(data)):
-        data[index]=data[index].split(" ")
+        data[index] = data[index].split(" ")
     numericDataFile.close()
-    #Merge everything
-    numericProblems=[]
-    try:#for dealing with the empty array problem
+    # Merge everything
+    numericProblems = []
+    try:  # for dealing with the empty array problem
         for index in range(len(questions)):
-            numericProblems.append({
-        "questions":questions[index],
-        "box":int(data[index][0]),
-        "answer":float(data[index][1]),
-        "significantFigures":int(data[index][2]),
-        "lastOpened":float(data[index][3]),
-        "errors":int(data[index][4])
-        })
+            numericProblems.append(
+                {
+                    "question": questions[index],
+                    "box": int(data[index][0]),
+                    "answer": float(data[index][1]),
+                    "significantFigures": int(data[index][2]),
+                    "lastOpened": float(data[index][3]),
+                    "errors": int(data[index][4]),
+                }
+            )
         return numericProblems
     except:
         print("Files: Empty numeric files")
-        return[]
-def obterExerciciosTeoricos():
-    fileEnunciadosTeoricos = open(".enunciadosTeoricos.txt","r",encoding="utf-8")
-    enunciados = fileEnunciadosTeoricos.read()
-    enunciados=enunciados.split("\n")
-    fileEnunciadosTeoricos.close()
-    fileGabaritosTeoricos =open(".gabaritosTeoricos.txt","r",encoding="utf-8")
-    gabaritos=fileGabaritosTeoricos.read()
-    gabaritos=gabaritos.split("\n")
-    fileGabaritosTeoricos.close()
-    fileDadosTeoricos =open(".dadosTeoricos.txt","r",encoding="utf-8")
-    dados=fileDadosTeoricos.read()
-    dados=dados.split("\n")
-    for index in range(len(dados)):
-        dados[index]= dados[index].split(" ")
-    fileDadosTeoricos.close()
-    exerciciosTeoricos=[]
+        return []
+
+
+def getTheoreticalProblems():
+    # Get Questions
+    theoreticalQuestionFile = open(
+        ".spacedRepetition/data/teoricQuestions", "a", encoding="utf-8"
+    )
+    questions = theoreticalQuestionFile.read()
+    questions = questions.split("\n")
+    theoreticalQuestionFile.close()
+    # Get Answers
+    theoreticalAnswerFile = open(
+        ".spacedRepetition/data/teoricAnswers", "a", encoding="utf-8"
+    )
+    answers = theoreticalAnswerFile.read()
+    answers = answers.split("\n")
+    theoreticalAnswerFile.close()
+    theoreticalDataFile = open(
+        ".spacedRepetition/data/teoricData", "a", encoding="utf-8"
+    )
+    data = theoreticalDataFile.read()
+    data = data.split("\n")
+    for index in range(len(data)):
+        data[index] = data[index].split(" ")
+    theoreticalDataFile.close()
+    # merge into a dictionary
+    theoreticalProblems = []
     try:
-        for index in range(len(enunciados)):
-            exerciciosTeoricos.append({
-            "enunciado":enunciados[index],
-            "caixa":int(dados[index][0]),
-            "gabarito":gabaritos[index],
-            "ultimaAbertura":float(dados[index][1]),
-            "erros":int(dados[index][2])
-        })
-        return exerciciosTeoricos
+        for index in range(len(questions)):
+            theoreticalProblems.append(
+                {
+                    "question": questions[index],
+                    "box": int(data[index][0]),
+                    "answer": answers[index],
+                    "lastOpened": float(data[index][1]),
+                    "error": int(data[index][2]),
+                }
+            )
+        return theoreticalProblems
     except:
-        print("Arquivos:Erro ao obter exercícios teóricos.")
-        return[]
+        print("Files: Empty numeric files")
+        return []
+
 
 def salvarExerciciosNumericos(exerciciosNumericos):
-    fileEnunciadosNumericos = open(".enunciadosNumericos.txt","w",encoding="utf-8")
-    fileDadosNumericos = open(".dadosNumericos.txt","w",encoding="utf-8")
+    fileEnunciadosNumericos = open(".enunciadosNumericos.txt", "w", encoding="utf-8")
+    fileDadosNumericos = open(".dadosNumericos.txt", "w", encoding="utf-8")
     for index in range(len(exerciciosNumericos)):
-        exercise= exerciciosNumericos[index]
-        fileEnunciadosNumericos.write( exercise["enunciado"])
-        fileDadosNumericos.write(str(exercise["caixa"])+" "+str(exercise["gabarito"])+" " + str(exercise["certeza"])+" "+str(exercise["ultimaAbertura"])+ " "+ str(exercise["erros"]))
-        if(index+1<len(exerciciosNumericos)):
+        exercise = exerciciosNumericos[index]
+        fileEnunciadosNumericos.write(exercise["enunciado"])
+        fileDadosNumericos.write(
+            str(exercise["caixa"])
+            + " "
+            + str(exercise["gabarito"])
+            + " "
+            + str(exercise["certeza"])
+            + " "
+            + str(exercise["ultimaAbertura"])
+            + " "
+            + str(exercise["erros"])
+        )
+        if index + 1 < len(exerciciosNumericos):
             fileEnunciadosNumericos.write("\n")
             fileDadosNumericos.write("\n")
     fileEnunciadosNumericos.close()
     fileDadosNumericos.close()
+
+
 def salvarExerciciosTeoricos(exerciciosAAdicionar):
-    fileEnunciadosTeoricos = open(".enunciadosTeoricos.txt","w",encoding="utf-8")
-    fileDadosTeoricos = open(".dadosTeoricos.txt","w",encoding="utf-8")
-    fileGabaritoTeoricos=open(".gabaritosTeoricos.txt","w",encoding="utf-8")
+    fileEnunciadosTeoricos = open(".enunciadosTeoricos.txt", "w", encoding="utf-8")
+    fileDadosTeoricos = open(".dadosTeoricos.txt", "w", encoding="utf-8")
+    fileGabaritoTeoricos = open(".gabaritosTeoricos.txt", "w", encoding="utf-8")
     for index in range(len(exerciciosAAdicionar)):
-        fileEnunciadosTeoricos.write( exerciciosAAdicionar[index]["enunciado"])
-        fileDadosTeoricos.write(str(exerciciosAAdicionar[index]["caixa"])+" "+str(exerciciosAAdicionar[index]["ultimaAbertura"])+ " "+ str(exerciciosAAdicionar[index]["erros"]))
+        fileEnunciadosTeoricos.write(exerciciosAAdicionar[index]["enunciado"])
+        fileDadosTeoricos.write(
+            str(exerciciosAAdicionar[index]["caixa"])
+            + " "
+            + str(exerciciosAAdicionar[index]["ultimaAbertura"])
+            + " "
+            + str(exerciciosAAdicionar[index]["erros"])
+        )
         fileGabaritoTeoricos.write(exerciciosAAdicionar[index]["gabarito"])
-        if(index+1<len(exerciciosAAdicionar)):
+        if index + 1 < len(exerciciosAAdicionar):
             fileEnunciadosTeoricos.write("\n")
             fileDadosTeoricos.write("\n")
             fileGabaritoTeoricos.write("\n")
     fileDadosTeoricos.close()
     fileDadosTeoricos.close()
 
-def adicionarExercicioNumerico(enunciado,gabarito,certeza,exerciciosAtuais):
-    exerciciosAtuais.append({
-        "enunciado":enunciado,
-        "caixa":0,
-        "gabarito":float(gabarito),
-        "certeza":int(certeza),
-        "ultimaAbertura":int(time.time()),
-        "erros":0
-        })
+
+def adicionarExercicioNumerico(enunciado, gabarito, certeza, exerciciosAtuais):
+    exerciciosAtuais.append(
+        {
+            "enunciado": enunciado,
+            "caixa": 0,
+            "gabarito": float(gabarito),
+            "certeza": int(certeza),
+            "ultimaAbertura": int(time.time()),
+            "erros": 0,
+        }
+    )
     salvarExerciciosNumericos(exerciciosAtuais)
-def deletarExercicioNumericos(index,exerciciosAtuais):
+
+
+def deletarExercicioNumericos(index, exerciciosAtuais):
     exerciciosAtuais.pop(index)
     salvarExerciciosNumericos(exerciciosAtuais)
     return exerciciosAtuais
-def adicionarExercicioTeorico(enunciado,gabarito,exerciciosAtuais):
-    exerciciosAtuais.append({
-        "enunciado":enunciado,
-        "caixa":0,
-        "gabarito":gabarito,
-        "ultimaAbertura":int(time.time()),
-        "erros":0
-        })
+
+
+def adicionarExercicioTeorico(enunciado, gabarito, exerciciosAtuais):
+    exerciciosAtuais.append(
+        {
+            "enunciado": enunciado,
+            "caixa": 0,
+            "gabarito": gabarito,
+            "ultimaAbertura": int(time.time()),
+            "erros": 0,
+        }
+    )
     salvarExerciciosTeoricos(exerciciosAtuais)
-def deletarExercicioTeorico(index,exerciciosAtuais):
+
+
+def deletarExercicioTeorico(index, exerciciosAtuais):
     exerciciosAtuais.pop(index)
     salvarExerciciosTeoricos(exerciciosAtuais)
     return exerciciosAtuais
+
+
 def listarExercicios():
-    exerciciosNumericos=obterExerciciosNumericos()
-    exerciciosTeoricos=obterExerciciosTeoricos()
-    exerciciosListados=["Exercícios Numéricos:"]
+    exerciciosNumericos = obterExerciciosNumericos()
+    exerciciosTeoricos = obterExerciciosTeoricos()
+    exerciciosListados = ["Exercícios Numéricos:"]
     for index in range(len(exerciciosNumericos)):
-        exercise=exerciciosNumericos[index]
-        exerciciosListados.append(str(index)+"-"+exercise["enunciado"]+"-caixa:"+str(exercise["caixa"])+"-erros:"+str(exercise["erros"]))
+        exercise = exerciciosNumericos[index]
+        exerciciosListados.append(
+            str(index)
+            + "-"
+            + exercise["enunciado"]
+            + "-caixa:"
+            + str(exercise["caixa"])
+            + "-erros:"
+            + str(exercise["erros"])
+        )
     exerciciosListados.append("Exercícios teóricos:")
     for index in range(len(exerciciosTeoricos)):
-        exercise=exerciciosTeoricos[index]
-        exerciciosListados.append(str(index)+"-"+exercise["enunciado"]+"-caixa:"+str(exercise["caixa"])+"-erros:"+str(exercise["erros"]))
+        exercise = exerciciosTeoricos[index]
+        exerciciosListados.append(
+            str(index)
+            + "-"
+            + exercise["enunciado"]
+            + "-caixa:"
+            + str(exercise["caixa"])
+            + "-erros:"
+            + str(exercise["erros"])
+        )
     exerciciosListados.append("finalizado")
     return exerciciosListados
+
+
 def listarExerciciosIncompletos():
-    exerciciosNumericos=obterExerciciosNumericos()
-    exerciciosTeoricos=obterExerciciosTeoricos()
-    exerciciosListados=["Exercícios Numéricos:"]
-    contador=0
+    exerciciosNumericos = obterExerciciosNumericos()
+    exerciciosTeoricos = obterExerciciosTeoricos()
+    exerciciosListados = ["Exercícios Numéricos:"]
+    contador = 0
     for index in range(len(exerciciosNumericos)):
-        exercise=exerciciosNumericos[index]
-        if exercise["caixa"]<4:
-            exerciciosListados.append(str(index)+"-"+exercise["enunciado"]+"-caixa:"+str(exercise["caixa"])+"-erros:"+str(exercise["erros"]))
-            contador+=1
+        exercise = exerciciosNumericos[index]
+        if exercise["caixa"] < 4:
+            exerciciosListados.append(
+                str(index)
+                + "-"
+                + exercise["enunciado"]
+                + "-caixa:"
+                + str(exercise["caixa"])
+                + "-erros:"
+                + str(exercise["erros"])
+            )
+            contador += 1
     exerciciosListados.append("Exercícios teóricos:")
     for index in range(len(exerciciosTeoricos)):
-        exercise=exerciciosTeoricos[index]
-        if exercise["caixa"]<4:
-            exerciciosListados.append(str(index)+"-"+exercise["enunciado"]+"-caixa:"+str(exercise["caixa"])+"-erros:"+str(exercise["erros"]))
-            contador+=1
-    exerciciosListados.append("finalizado:"+str(contador)+"exercícios incompletos")
+        exercise = exerciciosTeoricos[index]
+        if exercise["caixa"] < 4:
+            exerciciosListados.append(
+                str(index)
+                + "-"
+                + exercise["enunciado"]
+                + "-caixa:"
+                + str(exercise["caixa"])
+                + "-erros:"
+                + str(exercise["erros"])
+            )
+            contador += 1
+    exerciciosListados.append("finalizado:" + str(contador) + "exercícios incompletos")
     return exerciciosListados
+
+
 def conseguirAjuda():
-    #Leia o que está escrito em ajuda.txt
-    fileAjuda=open("help.txt","r",encoding="utf-8")
-    textoDeAjuda=fileAjuda.read()
+    # Leia o que está escrito em ajuda.txt
+    fileAjuda = open("help.txt", "r", encoding="utf-8")
+    textoDeAjuda = fileAjuda.read()
     fileAjuda.close()
     return textoDeAjuda
+
+
 initialiseFiles()
