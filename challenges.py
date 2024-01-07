ad54bfb4ebbf33316f58afade2e9982828c8970f
archPrag <arthur.prager.ea@gmail.com>
2024-01-07 11:41:49 -0300

    translated probability logic and fixed some names

--- challenges.py
+++ challenges.py
@@ -1,133 +1,166 @@
 # coding: utf-8
 import numpy as np
+
+
 def initializeFiles():
-    nameFile = open(".spacedRepetition/data/challenges/names.txt","a",encoding="utf-8")
+    nameFile = open(
+        ".spacedRepetition/data/challenges/names.txt", "a", encoding="utf-8"
+    )
     nameFile.close()
-    subdivisionsFile= open(".spacedRepetition/data/challenges/subdivisions.txt","a",encoding="utf-8")
+    subdivisionsFile = open(
+        ".spacedRepetition/data/challenges/subdivisions.txt", "a", encoding="utf-8"
+    )
     subdivisionsFile.close()
-    problemFile = open(".spacedRepetition/data/challenges/problems.txt","a",encoding="utf-8")
+    problemFile = open(
+        ".spacedRepetition/data/challenges/problems.txt", "a", encoding="utf-8"
+    )
     problemFile.close()
+
+
 def checkChallenges(materials):
     for material in materials:
-    #verifies if there are as many sections as exercises per section
-        if len(material["subdivisions"])!=len(material["exercises"]):
+        # verifies if there are as many sections as exercises per section
+        if len(material["subdivisions"]) != len(material["exercises"]):
             return False
     return True
-def integerStringCheck(checkedString:str):
+
+
+def integerStringCheck(checkedString: str):
     try:
         int(checkedString)
     except:
         return False
     return True
+
+
 def saveChallenges(materials):
     if not checkChallenges(materials):
         return False
-    #write the names
-    nameFile = open(".spacedRepetition/data/challenges/names.txt","w",encoding="utf-8")
+    # write the names
+    nameFile = open(
+        ".spacedRepetition/data/challenges/names.txt", "w", encoding="utf-8"
+    )
     for index in range(len(materials)):
         nameFile.write(materials[index]["name"])
-        if index+1<len(materials):
+        if index + 1 < len(materials):
             nameFile.write("\n")
     nameFile.close()
-    #write the subdivisions
-    subdivisionsFile= open(".spacedRepetition/data/challenges/subdivisions.txt","a",encoding="utf-8")
+    # write the subdivisions
+    subdivisionsFile = open(
+        ".spacedRepetition/data/challenges/subdivisions.txt", "a", encoding="utf-8"
+    )
     for index in range(len(materials)):
-        subdivisionsPH=""
+        subdivisionsPH = ""
         for indexPrime in range(len(materials[index]["subdivisions"])):
-            subdivisionsPH+=materials[index]["subdivision"][indexPrime]
-            if indexPrime+1<len(materials[index]["subdivisios"]):
-                subdivisionsPH+="@"
+            subdivisionsPH += materials[index]["subdivisions"][indexPrime]
+            if indexPrime + 1 < len(materials[index]["subdivisions"]):
+                subdivisionsPH += "@"
         subdivisionsFile.write(subdivisionsPH)
-        if index+1<len(materials):
+        if index + 1 < len(materials):
             subdivisionsFile.write("\n")
     subdivisionsFile.close()
-    #Write the number of problem
-    problemFile = open(".spacedRepetition/data/challenges/problems.txt","a",encoding="utf-8")
+    # Write the number of problem
+    problemFile = open(
+        ".spacedRepetition/data/challenges/problems.txt", "a", encoding="utf-8"
+    )
     for index in range(len(materials)):
-        problemPH=""
-        for indexPrime in range(len(materials[index]["exercise"])):
-            problemPH+=str(materials[index]["exercise"][indexPrime])
-            if indexPrime+1<len(materials[index]["exercise"]):
-                problemPH+=" "
+        problemPH = ""
+        for indexPrime in range(len(materials[index]["problems"])):
+            problemPH += str(materials[index]["problems"][indexPrime])
+            if indexPrime + 1 < len(materials[index]["problems"]):
+                problemPH += " "
         problemFile.write(problemPH)
-        if index+1<len(materials):
+        if index + 1 < len(materials):
             problemFile.write("\n")
     problemFile.close()
     return False
 
+
 def ConseguirDesafios():
     try:
-        #names
-        nameFile = open(".spacedRepetition/data/challenges/names.txt","r",encoding="utf-8")
-        namesPH=nameFile.read().split("\n")
+        # names
+        nameFile = open(
+            ".spacedRepetition/data/challenges/names.txt", "r", encoding="utf-8"
+        )
+        namesPH = nameFile.read().split("\n")
         nameFile.close()
-        #subdivisions
-        subdivisionsFile= open(".spacedRepetition/data/challenges/subdivisions.txt","r",encoding="utf-8")
-        subdivisionsPH=subdivisionsFile.read().split("\n")
+        # subdivisions
+        subdivisionsFile = open(
+            ".spacedRepetition/data/challenges/subdivisions.txt", "r", encoding="utf-8"
+        )
+        subdivisionsPH = subdivisionsFile.read().split("\n")
         for index in range(len(subdivisionsPH)):
-            subdivisionsPH[index]=subdivisionsPH[index].split("@");
+            subdivisionsPH[index] = subdivisionsPH[index].split("@")
         subdivisionsFile.close()
-        #number of problems
-        problemFile = open(".spacedRepetition/data/challenges/problems.txt","a",encoding="utf-8")
-        problemPH=problemFile.read().split("\n")
+        # number of problems
+        problemFile = open(
+            ".spacedRepetition/data/challenges/problems.txt", "a", encoding="utf-8"
+        )
+        problemPH = problemFile.read().split("\n")
         for index in range(len(problemPH)):
-            problemPH[index]=problemPH[index].split(" ")
+            problemPH[index] = problemPH[index].split(" ")
             for indexPrime in range(len(problemPH[index])):
-                problemPH[index][indexPrime]=int(problemsPH[index][indexPrime])
+                problemPH[index][indexPrime] = int(problemsPH[index][indexPrime])
         problemFile.close()
-        #Concatena o dicionário
-        materials=[]
+        # Concatena o dicionário
+        materials = []
         for index in range(len(namesPH)):
-            materials.append({
-            "names":namesPH[index],
-            "subdivisions":subdivisionsPH[index],
-            "exercicios":problemPH[index]
-        })
+            materials.append(
+                {
+                    "names": namesPH[index],
+                    "subdivisions": subdivisionsPH[index],
+                    "problems": problemPH[index],
+                }
+            )
         return materials
     except:
-        #here to incapsulate the empty problem
-        return[]
+        # here to incapsulate the empty problem
+        return []
 
 
-def probabilidade(materiais):
-    #Encontrando a distribuição de questoes
-    distribuicaoQuestoes=[]
-    for index in range(len(materiais)):
-        distribuicaoQuestoes.append(np.sum(np.array(materiais[index]["exercicios"])))
-    distribuicaoQuestoes=np.array(distribuicaoQuestoes)
-    print(distribuicaoQuestoes)
-    #Retorne caso só houver menos de 1 exercício
-    if len (distribuicaoQuestoes)==1:
+def probability(materials):
+    questionDistribution = []
+    for index in range(len(materials)):
+        questionDistribution.append(np.sum(np.array(materials[index]["problems"])))
+    questionDistribution = np.array(questionDistribution)
+    print(questionDistribution)
+    # return edge cases
+    if len(questionDistribution) == 1:
         return np.array([1])
-    elif len(distribuicaoQuestoes)==0:
+    elif len(questionDistribution) == 0:
         return np.array([0])
-    #Quero que o que tem mais questões tenha peso 2 e o que tem menos questões peso 1 os outros livros com  peso intermediário baseado no número de questões
-    #Encontrando os limites
-    minimo=np.min(distribuicaoQuestoes)
-    maximo=np.max(distribuicaoQuestoes)
-    #Encontre os pesos
-    pesos=(distribuicaoQuestoes-minimo)/(maximo-minimo)+1
-    #Encontre a probabilidade
-    return pesos/np.sum(pesos)
-def verificarPresencaArrouba(texto:str):
+    # The standard is the most heavy challendge book has twice the probability of the smallest and others have in betwens
+    # Encontrando os limites
+    minimum = np.min(questionDistribution)
+    maximum = np.max(questionDistribution)
+    # Encontre os pesos
+    notNormalizedProbability = (questionDistribution - minimum) / (maximum - minimum) + 1
+    # Encontre a probabilidade
+    return notNormalizedProbability / np.sum(notNormalizedProbability)
+
+
+def verificarPresencaArrouba(texto: str):
     for character in texto:
-        if character =="@":
+        if character == "@":
             return True
     return False
+
+
 def materiaisString(materiais):
-    textoFinal=["materiais randomizados :"]
+    textoFinal = ["materiais randomizados :"]
     for index in range(len(materiais)):
-        textoPH=str(index)+"-"+materiais[index]["nome"]
+        textoPH = str(index) + "-" + materiais[index]["nome"]
         for indexPrime in range(len(materiais[index]["exercicios"])):
-            textoPH+="\n  +"+materiais[index]["subdivisoes"][indexPrime]+"|"+str(materiais[index]["exercicios"][indexPrime])+" exercícios."
+            textoPH += (
+                "\n  +"
+                + materiais[index]["subdivisoes"][indexPrime]
+                + "|"
+                + str(materiais[index]["exercicios"][indexPrime])
+                + " exercícios."
+            )
         textoFinal.append(textoPH)
     textoFinal.append("pronto")
     return textoFinal
-
-
-
-
-
 
 
 initializeFiles()
